acad943766adf0f349088994b21ceafec9b62110
Adrian925492 <adrian.9254@gmail.com>
2020-03-14 15:59:35 +0100

    First test added - add to list

--- Source/List/SimpleListTest.cc
+++ Source/List/SimpleListTest.cc
@@ -1,7 +1,22 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(ListTest, SimpleListTest)
+#include "SimpleList.hpp"
+
+TEST(SimpleListTest, GetElementTest)
 {
-    EXPECT_TRUE(false);
+    /* GIVEN */
+    SimpleList_t<char> oList;
+
+    /* WHEN */
+    oList.insert(0, 'A');
+    oList.insert(1, 'B');
+    oList.insert(2, 'C');
+    oList.insert(3, 'D');
+
+    /* THEN */
+    EXPECT_EQ(oList.get(0), 'A');
+    EXPECT_EQ(oList.get(1), 'B');
+    EXPECT_EQ(oList.get(2), 'C');
+    EXPECT_EQ(oList.get(3), 'D');
 }
