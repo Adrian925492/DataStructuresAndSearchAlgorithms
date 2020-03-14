6971ad81afb9a47502efda7810684c766dc36424
Adrian925492 <adrian.9254@gmail.com>
2020-03-14 15:18:14 +0100

    updated

---
+++ Source/List/ListExample.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "SimpleList.hpp"
+
+using namespace std;
+
+
+void listExample(void)
+{
+    /* Simple list example */
+    cout << "Simple list example: /n/n";
+
+    SimpleList_t<unsigned char> simpleList;
+    
+
+
+}
