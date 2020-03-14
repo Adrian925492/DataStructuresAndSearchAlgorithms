d897a163b3459ed0b82eeef80a343ac7b4878d9f
Adrian925492 <adrian.9254@gmail.com>
2020-03-14 14:45:44 +0100

    update

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 int main(void)
 {
     std::cout << "Algorithms and Data structures" << std::endl << std::endl;
-
+    
     int i = 0;
+
+    while (i<5)
+    {
+        i++;
+    
+    }
 
     while (i<5)
     {
