94489cd99224b441aefcd73d8a4fdb3c6f950744
Adrian925492 <adrian.9254@gmail.com>
2020-03-15 14:57:13 +0100

    InitTestQueue

--- Source/Queue/DoubleSideQueue.hpp
+++ Source/Queue/DoubleSideQueue.hpp
@@ -6,6 +6,39 @@
 namespace doubleSideQueue
 {
 
+template<typename T>
+class Queue
+{
+public:
+    Queue(): _length(0) {}
+    ~Queue(){}
+
+    T front()
+    {
+        if (isEmpty())
+        {
+            return NULL;        //Check if queue is empty
+        }
+    }
+
+    T back()
+    {
+        if (isEmpty())
+        {
+            return NULL;        //Check if queue is empty
+        }
+    }
+
+    bool isEmpty()
+    {
+        return (_length == 0);  //Check if queue is empty
+    }
+private:
+    uint32_t _length;
+};
+
+
+
 }
 
 #endif
--- Source/Queue/DoubleSideQueueTest.cc
+++ Source/Queue/DoubleSideQueueTest.cc
@@ -7,5 +7,13 @@ using namespace doubleSideQueue;
 
 TEST(DoubleSideQueueTest, InitTest)
 {
-    EXPECT_TRUE(false);
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+
+    /* THEN */
+    EXPECT_TRUE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), NULL);
+    EXPECT_EQ(oQueue.back(), NULL);
 }
