a3e2f0450638f62c0bfc937e38e0d8364ceee40a
Adrian925492 <adrian.9254@gmail.com>
2020-03-15 14:44:09 +0100

    double side queue initial

---
+++ Source/Queue/DoubleSideQueue.hpp
@@ -0,0 +1,11 @@
+#ifndef __DOUBLE_SIDE_QUEUE_H_
+#define __DOUBLE_SIDE_QUEUE_H_
+
+#include <cstdio>
+
+namespace doubleSideQueue
+{
+
+}
+
+#endif
---
+++ Source/Queue/DoubleSideQueueTest.cc
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "DoubleSideQueue.hpp"
+
+using namespace doubleSideQueue;
+
+TEST(DoubleSideQueueTest, InitTest)
+{
+    EXPECT_TRUE(false);
+}
