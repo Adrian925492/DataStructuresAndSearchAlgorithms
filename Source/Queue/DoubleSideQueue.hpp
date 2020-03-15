3084eb77486d3ad5c531c16941531fbad1712fa5
Adrian925492 <adrian.9254@gmail.com>
2020-03-15 15:17:30 +0100

    DoubleListAdded

--- Source/Queue/DoubleSideQueue.hpp
+++ Source/Queue/DoubleSideQueue.hpp
@@ -6,19 +6,100 @@
 namespace doubleSideQueue
 {
 
+template <typename T>
+class Node
+{
+public:
+    Node(): value(NULL), next(nullptr), previous(nullptr) {}    
+    Node(T element): value(element), next(nullptr), previous(nullptr) {}
+    ~Node(){}
+
+    T value;
+    Node<T>* next;
+    Node<T>* previous;
+};
+
 template<typename T>
 class Queue
 {
 public:
-    Queue(): _length(0) {}
+    Queue(): _length(0), _front(nullptr), _back(nullptr) {}
     ~Queue(){}
 
-    T front()
+    void pushToFront(T element)
+    {
+        Node<T>* node = new Node<T>(element);  //Allocate memory for new node
+
+        if (isEmpty())  //Check if queue is empty
+        {
+            _front = node;              // New node is now front and back
+            _back = node;
+        }
+        else
+        {
+            _front->next = node;        //Link current front to new node
+
+            node->previous = _front;    //Lint new front node to current one
+
+            _front = node;              //Move front link to new node
+        }
+        _length++;      //Increment nr of items
+    }
+
+    void pushToBack(T element)
+    {
+        Node<T>* node = new Node<T>(element);  //Allocate memory for new node
+
+        if (isEmpty())  //Check if queue is empty
+        {
+            _front = node;              // New node is now front and back
+            _back = node;                   
+        }
+        else
+        {
+            _back->previous = node;     //Assign previous back node to new node
+
+            node->next = _back;         //Assign new node to previous back node
+
+            _back = node;               //Move back node pointer to new back node
+        }
+        _length++;      //Increment nr of items
+    }
+
+    void popFront()
     {
         if (isEmpty())
         {
-            return NULL;        //Check if queue is empty
+            return;        //Check if queue is empty
         }
+        
+        Node<T>* node = _front; //Save current front
+
+        _front = _front->previous;  //Move current front one backward
+
+        _front->next = nullptr;     //Set current front next to nullptr
+
+        delete node;            //Dealloc memory for last front
+
+        _length--;              //Decrement queue length
+    }
+
+    void popBack()
+    {
+        if (isEmpty())
+        {
+            return;        //Check if queue is empty
+        }
+        
+        Node<T>* node = _back; //Save current back
+
+        _back = _back->next;  //Move current front one backward
+
+        _back->previous = nullptr;  //Set current back previous to nullptr
+
+        delete node;            //Dealloc memory for last back
+
+        _length--;              //Decrement queue length
     }
 
     T back()
@@ -27,6 +108,16 @@ public:
         {
             return NULL;        //Check if queue is empty
         }
+        return _back->value;   //Return value
+    }
+
+    T front()
+    {
+        if (isEmpty())
+        {
+            return NULL;        //Check if queue is empty
+        }
+        return _front->value;   //Return value
     }
 
     bool isEmpty()
@@ -35,6 +126,8 @@ public:
     }
 private:
     uint32_t _length;
+    Node<T>* _front;
+    Node<T>* _back;
 };
 
 
--- Source/Queue/DoubleSideQueueTest.cc
+++ Source/Queue/DoubleSideQueueTest.cc
@@ -17,3 +17,69 @@ TEST(DoubleSideQueueTest, InitTest)
     EXPECT_EQ(oQueue.front(), NULL);
     EXPECT_EQ(oQueue.back(), NULL);
 }
+
+TEST(DoubleSideQueueTest, PushToFrontTest)
+{
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+    oQueue.pushToFront(1);
+    oQueue.pushToFront(2);
+    oQueue.pushToFront(3);
+
+    /* THEN */
+    EXPECT_FALSE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), 3u);
+    EXPECT_EQ(oQueue.back(), 1u);
+}
+
+TEST(DoubleSideQueueTest, PushToBackTest)
+{
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+    oQueue.pushToBack(1);
+    oQueue.pushToBack(2);
+    oQueue.pushToBack(3);
+
+    /* THEN */
+    EXPECT_FALSE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), 1u);
+    EXPECT_EQ(oQueue.back(),3u);
+}
+
+TEST(DoubleSideQueueTest, PopFrontTest)
+{
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+    oQueue.pushToFront(1);
+    oQueue.pushToFront(2);
+    oQueue.pushToFront(3);
+    oQueue.popFront();
+
+    /* THEN */
+    EXPECT_FALSE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), 2u);
+    EXPECT_EQ(oQueue.back(),1u);
+}
+
+TEST(DoubleSideQueueTest, PopBackTest)
+{
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+    oQueue.pushToFront(1);
+    oQueue.pushToFront(2);
+    oQueue.pushToFront(3);
+    oQueue.popBack();
+
+    /* THEN */
+    EXPECT_FALSE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), 3u);
+    EXPECT_EQ(oQueue.back(),2u);
+}
