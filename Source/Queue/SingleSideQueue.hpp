e1c7e2382483b7071c21a0ea6a20a7a59b2fb014
Adrian925492 <adrian.9254@gmail.com>
2020-03-15 13:49:24 +0100

    SingleSIdeQueueInitial added

---
+++ Source/Queue/SingleSideQueue.hpp
@@ -0,0 +1,6 @@
+#ifndef __SINGLE_SIDE_QUEUE_HPP_
+#define __SINGLE_SIDE_QUEUE_HPP_
+
+#include <cstdint>
+
+#endif
---
+++ Source/Queue/SingleSideQueueTest.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "SingleSideQueue.hpp"
+
+TEST(SingleSideQueueTest, IntiTest)
+{
+    EXPECT_TRUE(false);
+}
