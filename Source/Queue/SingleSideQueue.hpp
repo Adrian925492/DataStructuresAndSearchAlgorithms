3a5d83c0ee98dc8f1fd206df36507d8f12b7bc06
Adrian925492 <adrian.9254@gmail.com>
2020-03-15 14:18:07 +0100

    singleSideQueue_node,front method added

--- Source/Queue/SingleSideQueue.hpp
+++ Source/Queue/SingleSideQueue.hpp
@@ -7,11 +7,32 @@ namespace singleSideQueue
 {
 
 template <typename T>
+class Node
+{
+public:
+    Node(T element): value(element), next(nullptr){}
+    Node(): value(NULL), next(nullptr){}
+    ~Node(){}
+    T value;
+    Node<T>* next;
+};
+
+template <typename T>
 class Queue
 {
 public:
-    Queue(): _length(0) {}
+    Queue(): _length(0), _front(nullptr) {}
     ~Queue(){}
+
+    void enqueue(T element)
+    {
+        Node<T>* node = new Node<T>(element);   //Allocate space for new node
+        
+        if (_length == 0)       //Check if new node is the first one
+        {
+            _front = node;
+        }
+    }
 
     T front()
     {
@@ -19,6 +40,7 @@ public:
         {
             return NULL;    //Return NULL if queue is empty
         }
+        return _front->value;   //Return front node value
     }
 
     bool isEmpty()
@@ -27,6 +49,7 @@ public:
     }
 private:
     uint32_t _length;
+    Node<T>* _front;
 };
 
 }
--- Source/Queue/SingleSideQueueTest.cc
+++ Source/Queue/SingleSideQueueTest.cc
@@ -16,3 +16,17 @@ TEST(SingleSideQueueTest, IntiTest)
     EXPECT_TRUE(oQueue.isEmpty());
     EXPECT_EQ(oQueue.front(), NULL);
 }
+
+TEST(SingleSideQueueTest, EnqueueTest)
+{
+    /* GIVEN */
+    Queue<uint16_t> oQueue;
+
+    /* WHEN */
+    oQueue.enqueue(1u);
+    oQueue.enqueue(2u);
+
+    /* THEN */
+    EXPECT_FALSE(oQueue.isEmpty());
+    EXPECT_EQ(oQueue.front(), 1u);
+}
