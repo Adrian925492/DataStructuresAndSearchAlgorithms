8b83bc97a42e5e2fbc95b6e21e3a95c79172bc55
Adrian925492 <adrian.9254@gmail.com>
2020-03-23 17:43:47 +0100

    Binary search added

--- Source/BinarySearch/BinarySearchTest.cc
+++ Source/BinarySearch/BinarySearchTest.cc
@@ -7,12 +7,12 @@ using namespace binarySearch;
 
 TEST(SearchTest, binarySearchTest)
 {
-    int inputArray[5] = {1,2,5,6,4};
+    int inputArray[5] = {1,2,3,4,6};
 
     EXPECT_EQ(Search<int>(inputArray, 0, 5, 1), 0);
     EXPECT_EQ(Search<int>(inputArray, 0, 5, 2), 1);
-    EXPECT_EQ(Search<int>(inputArray, 0, 5, 5), 2);
-    EXPECT_EQ(Search<int>(inputArray, 0, 5, 6), 3);
-    EXPECT_EQ(Search<int>(inputArray, 0, 5, 4), 4);
+    EXPECT_EQ(Search<int>(inputArray, 0, 5, 3), 2);
+    EXPECT_EQ(Search<int>(inputArray, 0, 5, 4), 3);
+    EXPECT_EQ(Search<int>(inputArray, 0, 5, 6), 4);
     EXPECT_EQ(Search<int>(inputArray, 0, 5, 7), -1);   
 }
