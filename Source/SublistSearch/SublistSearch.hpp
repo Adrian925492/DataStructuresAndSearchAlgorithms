f99d543c0ac3224fdb4e37667c7fa9e262fac835
Adrian925492 <adrian.9254@gmail.com>
2020-03-25 10:28:04 +0100

    sublist search

--- Source/SublistSearch/SublistSearch.hpp
+++ Source/SublistSearch/SublistSearch.hpp
@@ -1,10 +1,60 @@
+#include "../List/List.hpp"
 
 namespace sublistSearch{
 
-template <typename T>
-int Search(T array[], int startIdx, int endIdx, T value)
+/* Sublist search
+
+The sublist search is different than other algorithms. It looks for sublist in other list, not for concrete
+element in container. The algorithm iterates threw input list nodes comparing if the node value is equal to 1st node value of 
+sublist looked for. If no, it goes to next node of list. If yes, it goes to next node of list and sublist and compares
+next node of sublist with node of the list. The comparison ends when sublist has no more nodes to compare and all
+nodes comparison were succesed (sublist searched - the algorithm teruens true), or if we go to end of input list and
+no sublist were searched (algorithm returns false than) */
+
+bool CompareAllMatchedElements(list::Node<int>* inputList, list::Node<int>* searchedSublist)
 {
-    return -1;
+    if (searchedSublist != nullptr && inputList == nullptr)
+    {
+        return false;       //Check if input list has not ended
+    }
+
+    if (searchedSublist == nullptr)
+    {
+        return true;        // searched list ended
+    }
+
+    if (inputList->value == searchedSublist->value)
+    {
+        return CompareAllMatchedElements(inputList->next, searchedSublist->next);   //Check next node
+    }
+    else
+    {
+        return false;       //Return false if not - fitted element found
+    }
+    
+}
+
+bool Search(list::Node<int>* inputList, list::Node<int>* searchedSublist)
+{
+    if (inputList == nullptr && searchedSublist == nullptr)
+    {
+        return true;    //Return true if both list are null - means were already compared
+    }
+
+    if (inputList == nullptr && searchedSublist != nullptr)
+    {
+        return false;   //Return false if inputlist has been searched and searched list has not been found
+    }
+ 
+    if (inputList->value == searchedSublist->value)
+    {
+        if (CompareAllMatchedElements(inputList, searchedSublist))
+        {
+            return true;        //Compare sublist if 1st element fits. Return true if coparison returned true.
+        }
+    }
+
+    return Search(inputList->next, searchedSublist);
 }
 
 }
--- Source/SublistSearch/SublistSearchTest.cc
+++ Source/SublistSearch/SublistSearchTest.cc
@@ -3,16 +3,24 @@
 
 #include "SublistSearch.hpp"
 
+#include "../List/List.hpp"
+
 using namespace sublistSearch;
 
 TEST(SearchTest, sublistSearchTest)
 {
-    int inputArray[5] = {1,2,5,6,4};
+    list::List<int> oInputList;
+    oInputList.insert(5, 0);
+    oInputList.insert(6, 1);
+    oInputList.insert(1, 2);
+    oInputList.insert(2, 3);
+    oInputList.insert(3, 4);
+    oInputList.insert(8, 5);
 
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 1), 0);
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 2), 1);
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 5), 2);
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 6), 3);
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 4), 4);
-    EXPECT_EQ(Search<int>(inputArray, 0, 4, 7), -1);   
+    list::List<int> oLookedForList;
+    oLookedForList.insert(1, 0);
+    oLookedForList.insert(2, 1);
+    oLookedForList.insert(3, 2);
+
+    EXPECT_TRUE(Search(oInputList.head, oLookedForList.head));
 }
