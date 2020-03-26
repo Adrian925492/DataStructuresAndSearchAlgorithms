2e830e798ad194f19471b5acaa1aed9495d338bd
Adrian925492 <adrian.9254@gmail.com>
2020-03-26 19:07:11 +0100

    update

--- Source/SearchingPattern/SearchingPatternTest.cc
+++ Source/SearchingPattern/SearchingPatternTest.cc
@@ -8,8 +8,6 @@ TEST(PatternSearch, PatternSearchTest)
     std::string inputString = "nie wiem czy tak czy nie";
     std::string target = "nie";
 
-    std::vector<int> expectedOutput(0,21);
-
     std::vector<int> output = patternSearch(inputString, target);
 
     EXPECT_EQ(output[0], 0);
