8beb077ad471f7be5c2e9d36a0988e346e5a8077
Adrian925492 <adrian.9254@gmail.com>
2020-03-27 09:59:00 +0100

    Binary tree

---
+++ Source/BinaryTree/BinaryTree.hpp
@@ -0,0 +1,19 @@
+
+
+namespace binaryTree{
+
+/* Binary tree
+
+Example of simple binary tree node. In binary tree each node can have only 2 child (up to) - left child and right child.
+*/
+
+class Node
+{
+public:
+    int key;
+    Node* leftChild;
+    Node* rightChild;
+};
+
+
+}
