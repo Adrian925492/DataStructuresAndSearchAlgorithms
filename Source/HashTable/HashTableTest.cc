df9d8631f37e9787d92e5359a385c393bb9dc234
Adrian925492 <adrian.9254@gmail.com>
2020-03-29 13:39:10 +0200

    HashTable_openAdress

--- Source/HashTable/HashTableTest.cc
+++ Source/HashTable/HashTableTest.cc
@@ -2,6 +2,7 @@
 #include <gmock/gmock.h>
 
 #include "HashTableChain.hpp"
+#include "HashTableOpenAdress.hpp"
 
 TEST(HashTableChain, InitTest)
 {
@@ -53,3 +54,52 @@ TEST(HashTableChain, RemoveTest)
     EXPECT_EQ(oHashTable.search(895), "");
 }
 
+TEST(HashTableOpenAdress, InitTest)
+{
+    /* GIVEN */
+    ::openAdressMethod::HashTable oHashTable;
+
+    /* WHEN */
+
+    /* THEN */
+    EXPECT_TRUE(oHashTable.isEmpty());
+}
+
+TEST(HashTableOpenAdress, InsertSearchTest)
+{
+    /* GIVEN */
+    ::openAdressMethod::HashTable oHashTable;
+
+    /* WHEN */
+    oHashTable.insert(128, "Opel");
+    oHashTable.insert(332, "Skoda");
+    oHashTable.insert(391, "Fiat");
+    oHashTable.insert(895, "Toyota");   //391 and 895 has collision if hash table size is 7
+
+    /* THEN */
+    EXPECT_FALSE(oHashTable.isEmpty());
+    EXPECT_EQ(oHashTable.search(128), "Opel");
+    EXPECT_EQ(oHashTable.search(332), "Skoda");
+    EXPECT_EQ(oHashTable.search(391), "Fiat");
+    EXPECT_EQ(oHashTable.search(895), "Toyota");
+}
+
+TEST(HashTableOpenAdress, RemoveTest)
+{
+    /* GIVEN */
+    ::openAdressMethod::HashTable oHashTable;
+
+    /* WHEN */
+    oHashTable.insert(128, "Opel");
+    oHashTable.insert(332, "Skoda");
+    oHashTable.insert(391, "Fiat");
+    oHashTable.insert(895, "Toyota");   //391 and 895 has collision if hash table size is 7
+    oHashTable.remove(895);
+
+    /* THEN */
+    EXPECT_FALSE(oHashTable.isEmpty());
+    EXPECT_EQ(oHashTable.search(128), "Opel");
+    EXPECT_EQ(oHashTable.search(332), "Skoda");
+    EXPECT_EQ(oHashTable.search(391), "Fiat");
+    EXPECT_EQ(oHashTable.search(895), "");
+}
